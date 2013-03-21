759da82409ada638c9e2c78282b377484fda7689
Jack Ketcham <kcajketcham@gmail.com>
2013-03-21 07:54:10 -0700

    Uncache CSS because it sucks

--- game/default.php
+++ game/default.php
@@ -30,9 +30,9 @@ $revisit = false;
 
     <title>Play - Team Tyro</title>
 
-    <link rel="stylesheet" href="/ext/css/foundation.min.css" />
+    <link rel="stylesheet" href="/ext/css/foundation.min.css?v=1.0" />
     <link rel="stylesheet" href="/ext/css/normalize.css" />
-    <link rel="stylesheet" href="/ext/css/app.css" />
+    <link rel="stylesheet" href="/ext/css/app.css?v=1.0" />
 
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="/ext/js/jquery-1.9.0.min.js"><\/script>')</script>
