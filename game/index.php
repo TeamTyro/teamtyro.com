d3b0e610c98bd059e5eb1b3e720be0fd0b8cfbeb
Jack Ketcham <kcajketcham@gmail.com>
2013-04-01 08:47:01 -0700

    Rename "name"cookie & add "ethnicity" cookie
    Also check that the non-mandatory "email" cookie exists before setting
    the variable

--- game/index.php
+++ game/index.php
@@ -2,11 +2,14 @@
 
 ob_start();
 
-if(isset($_COOKIE['name'])){
+if(isset($_COOKIE['survey_name'])){
     $name = $_COOKIE['survey_name'];
-    $email = $_COOKIE['survey_email'];
+    if(isset($_COOKIE['survey_email'])){
+        $email = $_COOKIE['survey_email'];
+    }
     $gender = $_COOKIE['survey_gender'];
     $age = $_COOKIE['survey_age'];
+    $ethnicity = $_COOKIE['survey_ethnicity'];
 }
 
 ?>
