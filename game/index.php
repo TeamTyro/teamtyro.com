2be71449a5ec6a4e8fa60b96ab5279e2c9d8f6ea
Jack Ketcham <kcajketcham@gmail.com>
2013-04-01 08:49:42 -0700

    Add value to email if not given

--- game/index.php
+++ game/index.php
@@ -6,6 +6,8 @@ if(isset($_COOKIE['survey_name'])){
     $name = $_COOKIE['survey_name'];
     if(isset($_COOKIE['survey_email'])){
         $email = $_COOKIE['survey_email'];
+    } else {
+        $email = 0;
     }
     $gender = $_COOKIE['survey_gender'];
     $age = $_COOKIE['survey_age'];
