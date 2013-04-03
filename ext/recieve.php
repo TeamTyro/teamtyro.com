a96be8c9127aa6b3875b0fb2694e43edd03bc151
Jack Ketcham <kcajketcham@gmail.com>
2013-04-02 20:36:38 -0700

    PHP include error trying to fix

--- ext/recieve.php
+++ ext/recieve.php
@@ -1,7 +1,9 @@
 <?php
 
     include("/home/c0smic/secure/data_db_settings.php");
-    include("$_SERVER['DOCUMENT_ROOT'] . /game/survey_variables.php");
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/game/survey_variables.php";
+    include($path);
 
     $body = file_get_contents('php://input');
 
