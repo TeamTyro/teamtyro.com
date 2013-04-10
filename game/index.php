ed65f2df903aa1e6f82ab76aefd2401f063116a1
Jack Ketcham <kcajketcham@gmail.com>
2013-04-09 22:50:38 -0700

    Create session to pass into database

--- game/index.php
+++ game/index.php
@@ -1,6 +1,7 @@
 <?php
 
 ob_start();
+session_start();
 
 include("survey_variables.php");
 
--- game/recieve.php
+++ game/recieve.php
@@ -3,12 +3,21 @@
     include("/home/c0smic/secure/data_db_settings.php");
     include("survey_variables.php");
 
+    session_start();
+    if (!isset($_SESSION['plays'])) { 
+        $_SESSION['plays'] == 0;
+    } else {
+        $_SESSION['plays'] += 1;
+    }
+
     $body = file_get_contents('php://input');
 
     setcookie('game_first-run', true, 5184000 + time(), '/');
 
     $ttime = "";
     $moves = "";
+    $session = session_id();
+    $return = $_SESSION['plays'];
 
     function parseData($body) {
         $splitter = substr($body, 0, 1);
@@ -32,13 +41,13 @@
     mysql_select_db('c0smic_maze-game');
 
     if(isset($_COOKIE['game_first-run'])){
-        $sql= "INSERT INTO game_returning (email, gender, age, ethnicity, ttime, moves)
+        $sql= "INSERT INTO game_returning (session, return, email, gender, age, ethnicity, ttime, moves)
         VALUES
-        ('$email', '$gender', '$age', '$ethnicity', '$ttime', '$moves')";
+        ('$session', '$return', '$email', '$gender', '$age', '$ethnicity', '$ttime', '$moves')";
     } else {
-        $sql= "INSERT INTO game_first_run (email, gender, age, ethnicity, ttime, moves)
+        $sql= "INSERT INTO game_first_run (session, return, email, gender, age, ethnicity, ttime, moves)
         VALUES
-        ('$email', '$gender', '$age', '$ethnicity', '$ttime', '$moves')";
+        ('$session', '$return', '$email', '$gender', '$age', '$ethnicity', '$ttime', '$moves')";
     }
 
     $retval = mysql_query( $sql, $conn );
